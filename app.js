fae32027a1f86e43ef97f5292470f3f80c769e74
huangdylan08 <huang.dy@northeastern.edu>
2022-11-20 21:33:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.use(express.json());
 TuitsController(app);
 HelloController(app)
 UserController(app)
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000, function() {console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);})
